fbf47a301bd7e69bd0dc6403572e05ef73d5ec6b
Denis <sciamano@users.noreply.github.com>
2022-06-16 11:03:13 +0200

    add unit test

---
+++ mytest.cpp
@@ -0,0 +1,19 @@
+#include "track.h"
+
+#include <QTest>
+
+class TestLoopBuffer : public QObject
+{
+  Q_OBJECT
+private slots:
+  void bufferSizeTest();
+};
+
+void TestLoopBuffer::bufferSizeTest()
+{
+  auto* buffer = new LoopBuffer(this);
+  QCOMPARE(buffer->size(), std::numeric_limits<qint64>::max());
+}
+
+QTEST_MAIN(TestLoopBuffer)
+#include "mytest.moc"
