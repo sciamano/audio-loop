39ab703f2331f3b76d8aff6131caddd172d509fb
Denis <sciamano@users.noreply.github.com>
2023-03-19 09:42:09 +0100

    dpi info

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -33,10 +33,47 @@ MainWindow::MainWindow(QWidget* parent) :
 
   auto widget = new QWidget(this);
   auto layout = new QVBoxLayout(widget);
-  auto control1 = new Control1(this);
-  layout->addWidget(control1, 0, Qt::AlignmentFlag::AlignLeft);
-  auto control2 = new Control1(this);
-  layout->addWidget(control2, 0, Qt::AlignmentFlag::AlignLeft);
+
+  {
+      auto logicalDPI = new QLabel(QString("Logical DPI: %1").arg(logicalDpiX()));
+      layout->addWidget(logicalDPI, 0, Qt::AlignmentFlag::AlignLeft);
+  }
+
+  {
+      auto physicalDPI = new QLabel(QString("Physical DPI: %1").arg(physicalDpiX()));
+      layout->addWidget(physicalDPI, 0, Qt::AlignmentFlag::AlignLeft);
+  }
+
+  {
+      auto pixelRatio = new QLabel(QString("Pixel Ratio: %1").arg(devicePixelRatioF()));
+      layout->addWidget(pixelRatio, 0, Qt::AlignmentFlag::AlignLeft);
+  }
+
+  {
+      auto screenLogicalDPI = new QLabel(QString("Screen Logical DPI: %1").arg(screen()->logicalDotsPerInch()));
+      layout->addWidget(screenLogicalDPI, 0, Qt::AlignmentFlag::AlignLeft);
+  }
+
+  {
+      auto screenPhysicalDPI = new QLabel(QString("Screen Physical DPI: %1").arg(screen()->physicalDotsPerInch()));
+      layout->addWidget(screenPhysicalDPI, 0, Qt::AlignmentFlag::AlignLeft);
+  }
+
+  {
+      auto screenPixelRatio = new QLabel(QString("Screen Pixel Ratio: %1").arg(screen()->devicePixelRatio()));
+      layout->addWidget(screenPixelRatio, 0, Qt::AlignmentFlag::AlignLeft);
+  }
+
+  {
+      auto control1 = new Control1(this);
+      layout->addWidget(control1, 0, Qt::AlignmentFlag::AlignLeft);
+  }
+
+  {
+      auto control2 = new Control1(this);
+      layout->addWidget(control2, 0, Qt::AlignmentFlag::AlignLeft);
+  }
+
   setCentralWidget(widget);
 }
 
